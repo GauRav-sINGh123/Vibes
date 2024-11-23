9045a02933e89a38a2373401a6a91f2a12c5b46e
Gaurav Singh <gksingh947@gmail.com>
2024-11-23 23:26:39 +0530

    User Model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,1 +1,41 @@
-import {Schema, model} from "mongoose";
+import {Model, Schema, model, models} from "mongoose";
+
+export interface User extends Document{
+    clerkId:string,
+    email:string,
+    username:string,
+    firstName:string,
+    lastName:string,
+    image:string
+}
+
+const userSchema:Schema<User>=new Schema({
+    clerkId:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    username:{
+        type:String,
+        required:true
+    },
+    firstName:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    }
+})
+
+const User=(models.User as Model<User>) || model<User>('User',userSchema)
+
+export default User
