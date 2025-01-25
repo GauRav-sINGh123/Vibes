e3b596116226d043d1d3c391911c6fa385ed5396
Gaurav Singh <gksingh947@gmail.com>
2025-01-25 15:19:59 +0530

    Update User Action Created

--- src/actions/user.actions.ts
+++ src/actions/user.actions.ts
@@ -2,7 +2,7 @@
 import connectDB from "@/database/db"
 import { handleError } from "@/lib/utils";
 import User from "@/models/user.model";
-import {CreateUserParams} from "@/types/index"
+import {CreateUserParams, UpdateUserParams} from "@/types/index"
 
 export const createUser=async(user:CreateUserParams)=>{
     try {
@@ -14,3 +14,17 @@ export const createUser=async(user:CreateUserParams)=>{
         handleError(error);
     }
 }
+
+export async function updateUser(clerkId: string, user: UpdateUserParams) {
+    try {
+      await connectDB();
+  
+      const updatedUser = await User.findOneAndUpdate({ clerkId }, user, { new: true })
+  
+      if (!updatedUser) throw new Error('User update failed')
+      return JSON.parse(JSON.stringify(updatedUser))
+    } catch (error) {
+      handleError(error)
+    }
+  }
+  
