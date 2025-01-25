34e3194e4a16aa97897b92b1d1b9c697faa5401d
Gaurav Singh <gksingh947@gmail.com>
2025-01-25 15:28:50 +0530

    User Update Event Webhook Added

--- src/actions/user.actions.ts
+++ src/actions/user.actions.ts
@@ -3,6 +3,7 @@ import connectDB from "@/database/db"
 import { handleError } from "@/lib/utils";
 import User from "@/models/user.model";
 import {CreateUserParams, UpdateUserParams} from "@/types/index"
+import { revalidatePath } from "next/cache";
 
 export const createUser=async(user:CreateUserParams)=>{
     try {
@@ -27,4 +28,24 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
       handleError(error)
     }
   }
+
+
+  export async function deleteUser(clerkId: string) {
+    try {
+      await connectDB();
+      // Find user to delete
+      const userToDelete = await User.findOne({ clerkId })
   
+      if (!userToDelete) {
+        throw new Error('User not found')
+      }
+    
+      // Delete user
+      const deletedUser = await User.findByIdAndDelete(userToDelete._id)
+      revalidatePath('/')
+  
+      return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null
+    } catch (error) {
+      handleError(error)
+    }
+  }
